ed22b7e81906037aa188531f74a2fa0bb4f1f7b4
Daniel Berger <dberger@globe.gov>
2010-09-21 17:19:05 -0600

    Added ability to get belongs_to relationships.

--- lib/oracle/model/generator.rb
+++ lib/oracle/model/generator.rb
@@ -7,6 +7,10 @@ module Oracle
 
       attr_reader :connection
       attr_reader :constraints
+      attr_reader :foreign_keys
+      attr_reader :belongs_to
+      attr_reader :table
+      attr_reader :view
 
       # Example:
       #
@@ -18,10 +22,16 @@ module Oracle
         @connection   = connection
         @constraints  = []
         @primary_keys = []
+        @foreign_keys = []
+        @belongs_to   = []
+        @table        = nil
       end
 
       def generate(table, view = false)
+        @table = table
+        @view  = view
         get_constraints(table) unless view
+        get_foreign_keys unless view
       end
 
       private
@@ -29,7 +39,7 @@ module Oracle
       # Get the primary key. If there is more than one, then an array of
       # keys are returned.
       #
-      def primary_keys
+      def get_primary_keys
         @constraints.each{ |hash|
           if hash['CONSTRAINT_TYPE'] == 'P'
             @primary_keys << hash['COLUMN_NAME'].downcase
@@ -41,6 +51,39 @@ module Oracle
         else
           @primary_keys[0].to_sym
         end
+      end
+
+      def get_foreign_keys
+        @constraints.each{ |hash|
+          if hash['CONSTRAINT_TYPE'] == 'R'
+            @foreign_keys << hash['R_CONSTRAINT_NAME']
+          end
+        }
+
+        get_belongs_to()
+      end
+
+      def get_belongs_to
+        @foreign_keys.each{ |fk|
+          @belongs_to << find_fk_table(fk)
+        }
+      end
+
+      def find_fk_table(fk)
+        sql = %Q{
+          select table_name
+          from all_constraints
+          where constraint_name = '#{fk}'
+        }
+
+        begin
+          cursor = @connection.exec(sql)
+          fk = cursor.fetch.first
+        ensure
+          cursor.close
+        end
+
+        fk
       end
 
       # Get a list of constraints for a given table.
