69dcfb85459aafbedc0c363f90b413f8110633bf
Daniel Berger <78529+djberg96@users.noreply.github.com>
2025-07-23 14:08:28 -0400

    Assume we're running from the base project directory, fix perms issue.

--- docker/sqlserver/setup-db.sh
+++ docker/sqlserver/setup-db.sh
@@ -4,6 +4,6 @@
 sleep 30s
 
 # Run the initialization script
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i /usr/src/app/init-db.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i /opt/mssql-tools/scripts/init-db.sql
 
 echo "Database initialization completed"
